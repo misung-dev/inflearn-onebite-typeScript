492540da9db904e4a47a4da79a560a5bbbf71480
misung.dev <misung.dev@gmail.com>
2025-01-31 00:37:13 +0900

    원시타입과 리터럴타입

---
+++ section02/src/chapter1.ts
@@ -0,0 +1,29 @@
+// number
+let num1: number = 123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+
+// null
+let null1: null = null;
+
+//undefined
+let unde1: undefined = undefined;
+
+// 임시적으로 numA에 null을 넣어둠
+//  "strictNullChecks": false, 설정해둬서 null이 가능함
+// let numA: number = null;
+
+// 리터럴 타입 (값으로도 타입을 정의 가능)
+// numA = 12; // 10만 가능하도록 만듦
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
