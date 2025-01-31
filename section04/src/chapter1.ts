579bbad3e1f7da8485fc7766faec7d1f2fcb0c03
misung.dev <misung.dev@gmail.com>
2025-01-31 23:05:27 +0900

    함수 타입 표현식과 호출 시그니쳐

---
+++ section04/src/chapter1.ts
@@ -0,0 +1,20 @@
+// 함수 타입 표현식
+// 일일히 타입을 지정하지 않아도 되는 장점 (중복 제거)
+type Operation = (a: number, b: number) => number;
+
+const add: Operation = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const multiply: Operation = (a, b) => a * b;
+const divide: Operation = (a, b) => a / b;
+
+// 호출 시그니처 (콜 시그니처)
+type Operation2 = {
+  (a: number, b: number): number;
+};
+
+function func(a: number): void {}
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
